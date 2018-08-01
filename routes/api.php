58d5f724794c85dec611fc1262ecf851d557a1f9
Yosmangel Adames <yosmangel_yk@hotmail.com>
2018-07-31 22:48:14 -0400

    rutas iniciales para el proyecto

--- routes/api.php
+++ routes/api.php
@@ -13,5 +13,34 @@ use Illuminate\Http\Request;
 |
 */
 
+/**
+ * Buyers
+ */
+Route::resource('buyers','Buyer\BuyerController',['only'=>['index','show']]);
+
+/**
+ * Categories
+ */
+Route::resource('categories','Category\CategoryController',['except'=>['create','edit']]);
+
+/**
+ * Products
+ */
+Route::resource('products','Product\ProductController',['only'=>['index','show']]);
+
+/**
+ * Transactions
+ */
+Route::resource('transactions','Transaction\TransactionController',['only'=>['index','show']]);
+
+/**
+ *Sellers 
+ */
+Route::resource('sellers','Seller\SellerController',['only'=>['index','show']]);
+
+/**
+ * Users
+ */
+Route::resource('users','User\UserController',['except'=>['create','edit']]);
 
 
