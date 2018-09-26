74815cf98e29a4cd49d0429234344724fc96f351
Yosmangel Adames <yosmangel_yk@hotmail.com>
2018-09-26 17:07:19 +0200

    Paginacion

--- app/Traits/ApiResponse.php
+++ app/Traits/ApiResponse.php
@@ -4,6 +4,8 @@ namespace App\Traits;
 
 use Illuminate\Support\Collection;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Pagination\LengthAwarePaginator;
 
 trait ApiResponse{
 
@@ -25,6 +27,7 @@ trait ApiResponse{
 
 		$collection=$this->filterData($collection, $transformer);
 		$collection=$this->sortData($collection, $transformer);
+		$collection=$this->paginate($collection);
 		$collection=$this->transformData($collection,$transformer);
 
 		return $this->successResponse($collection,$code);
@@ -55,7 +58,34 @@ trait ApiResponse{
 			}
 		}
 
-		return $colection;
+		return $collection;
+	}
+
+	protected function paginate(Collection $collection){
+		
+		$rules= [
+			'per_page'=> 'integer|min:2|max:50'
+		];
+
+		Validator::validate(request()->all(),$rules);
+
+
+		$page=LengthAwarePaginator::resolveCurrentPage();
+
+		$perPage=15;
+
+		if(request()->has('per_page')){
+			$perPage= (int)request()->per_page;
+		}
+		$results= $collection->slice(($page-1)*$perPage, $perPage)->values();
+
+		$paginated= new LengthAwarePaginator($results, $collection->count(), $perPage, $page, [
+			'path'=> LengthAwarePaginator::resolveCurrentPath(),
+		]);
+
+		$paginated->appends(request()->all());
+
+		return $paginated;
 	}
 
 	protected function sortData(Collection $collection, $transformer){
